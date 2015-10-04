40c37321691dfbe1b31c9158a1fdf779e23d034f
Juozas Rimša <juozas.rimsa@gmail.com>
2015-10-04 22:32:01 +0300

    Add unmarshalling tests

---
+++ ndc-client/src/test/java/org/iata/iata/edist/AirShoppingRqUnmarshallingTest.java
@@ -0,0 +1,59 @@
+package org.iata.iata.edist;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.InputStream;
+import java.util.*;
+
+import javax.xml.bind.*;
+
+import org.iata.iata.edist.AirShopReqAttributeQueryType.OriginDestination;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class AirShoppingRqUnmarshallingTest {
+
+	@Parameters(name = "{index}: {0} {1}")
+	public static Collection<String[]> sampleFiles() {
+		return Arrays.asList(new String[][] {
+			{"/AirShoppingRQ - OneWay with all Cabin.xml", "PRG"},
+			{"/AirShoppingRQ - OneWay with Calendar.xml", "PRG"},
+			{"/AirShoppingRQ - OneWay with Direct Flight.xml", "FRA"},
+			{"/AirShoppingRQ - OneWay with multiple pax - Direct Flight.xml", "FRA"},
+			{"/AirShoppingRQ - OneWay with multiple pax.xml", "PRG"},
+			{"/AirShoppingRQ - OneWay with one pax.xml", "PRG"},
+			{"/AirShoppingRQ - RoundTrip with all Cabin.xml", "BCN"},
+			{"/AirShoppingRQ - RoundTrip with Calendar - Direct Flight.xml", "CDG"},
+			{"/AirShoppingRQ - RoundTrip with Calendar.xml", "BCN"},
+			{"/AirShoppingRQ - RoundTrip with Direct Flight.xml", "CDG"},
+			{"/AirShoppingRQ - RoundTrip with multiple pax.xml", "BCN"},
+			{"/AirShoppingRQ - RoundTrip with one pax.xml", "BCN"}
+		});
+	}
+
+	@Parameter
+	public String resource;
+
+	@Parameter(value=1)
+	public String arrivalAirport;
+
+	@Test
+	public void unmarshal() {
+		InputStream is = this.getClass().getResourceAsStream(resource);
+		try {
+			JAXBContext context = JAXBContext.newInstance(AirShoppingRQ.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			AirShoppingRQ airShoppingRQ =  (AirShoppingRQ) unmarshaller.unmarshal(is);
+			List<OriginDestination> originDestinations = airShoppingRQ.getCoreQuery().getOriginDestinations().getOriginDestination();
+			assertEquals(arrivalAirport, originDestinations.get(0).getArrival().getAirportCode().getValue());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+	}
+}
