a0832d22e8e1b7b789f52b0731432ace6b4efe90
Juozas Rimša <juozas.rimsa@gmail.com>
2015-10-11 01:42:41 +0300

    Add OrderViewRS unmarshalling test

---
+++ ndc-client/src/test/java/org/iata/iata/edist/OrderViewRSUnmarshallingIT.java
@@ -0,0 +1,50 @@
+package org.iata.iata.edist;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.xml.bind.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class OrderViewRSUnmarshallingIT {
+
+	@Parameters(name = "{index}: {0} {1}")
+	public static Collection<String[]> sampleFiles() {
+		return Arrays.asList(new String[][] {
+			{"/OrderViewRS for OrderCreate - OneWay with one pax.xml", "1#C#109987187#110031494"},
+			{"/OrderViewRS for OrderCreate - Roundtrip with one pax.xml", "1#M#108183266"},
+			{"/OrderViewRS of OrderChange  - AddPassenger - OneWay.xml", "1#C#109987187#110031494"},
+			{"/OrderViewRS of OrderChange  - AddPassenger - RoundTrip.xml", "1#M#108183266"}
+		});
+	}
+
+	@Parameter
+	public String resource;
+
+	@Parameter(value=1)
+	public String offerItemID;
+
+	@Test
+	public void unmarshal() {
+		InputStream is = this.getClass().getResourceAsStream(resource);
+		try {
+			JAXBContext context = JAXBContext.newInstance(OrderViewRS.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			OrderViewRS orderViewRS =  (OrderViewRS) unmarshaller.unmarshal(is);
+			assertEquals(offerItemID, orderViewRS.getResponse().getOrder().getOrderItems().getOrderItem().get(0).getOrderItemID().getValue());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+	}
+}
