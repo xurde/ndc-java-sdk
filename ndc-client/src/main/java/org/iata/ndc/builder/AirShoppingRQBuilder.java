61ff5380ba0a5f7f0d8ee11ed3881e6f1318b834
Juozas Rimša <juozas.rimsa@gmail.com>
2015-10-10 18:06:35 +0300

    Fix indentation for byteToHex

--- ndc-client/src/main/java/org/iata/ndc/builder/AirShoppingRQBuilder.java
+++ ndc-client/src/main/java/org/iata/ndc/builder/AirShoppingRQBuilder.java
@@ -211,15 +211,13 @@ public class AirShoppingRQBuilder {
 		return xmlgc;
 	}
 
-	private static String byteToHex(final byte[] hash)
-	{
-	    Formatter formatter = new Formatter();
-	    for (byte b : hash)
-	    {
-	        formatter.format("%02x", b);
-	    }
-	    String result = formatter.toString();
-	    formatter.close();
-	    return result;
+	private static String byteToHex(final byte[] hash) {
+		Formatter formatter = new Formatter();
+		for (byte b : hash) {
+			formatter.format("%02x", b);
+		}
+		String result = formatter.toString();
+		formatter.close();
+		return result;
 	}
 }
