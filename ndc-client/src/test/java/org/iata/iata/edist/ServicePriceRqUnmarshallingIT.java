cc9eb93227bb9757aeef14f5083995ccde382da8
Juozas Rimša <juozas.rimsa@gmail.com>
2015-10-12 20:43:23 +0300

    Add unmarshalling tests for ServicePriceRQ and ServicePriceRS

---
+++ ndc-client/src/test/java/org/iata/iata/edist/ServicePriceRqUnmarshallingIT.java
@@ -0,0 +1,47 @@
+package org.iata.iata.edist;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.xml.bind.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class ServicePriceRqUnmarshallingIT {
+
+	@Parameters(name = "{index}: {0}")
+	public static Collection<String[]> sampleFiles() {
+		return Arrays.asList(new String[][] {
+			{"/ServicePriceRQ.xml", "REeda594f0d2a740b9a6d210dd463cc987"}
+		});
+	}
+
+	@Parameter
+	public String resource;
+
+	@Parameter(value=1)
+	public String responseID;
+
+	@Test
+	public void unmarshal() {
+		InputStream is = this.getClass().getResourceAsStream(resource);
+		try {
+			JAXBContext context = JAXBContext.newInstance(ServicePriceRQ.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			ServicePriceRQ servicePrice =  (ServicePriceRQ) unmarshaller.unmarshal(is);
+			assertEquals(responseID, servicePrice.getShoppingResponseID().getResponseID().getValue());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+	}
+}
---
+++ ndc-client/src/test/java/org/iata/iata/edist/ServicePriceRsUnmarshallingIT.java
@@ -0,0 +1,48 @@
+package org.iata.iata.edist;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.xml.bind.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class ServicePriceRsUnmarshallingIT {
+
+	@Parameters(name = "{index}: {0}")
+	public static Collection<Object[]> sampleFiles() {
+		return Arrays.asList(new Object[][] {
+			{"/ServicePriceRS.xml", BigDecimal.valueOf(170)}
+		});
+	}
+
+	@Parameter
+	public String resource;
+
+	@Parameter(value=1)
+	public BigDecimal expectedAmount;
+
+	@Test
+	public void unmarshal() {
+		InputStream is = this.getClass().getResourceAsStream(resource);
+		try {
+			JAXBContext context = JAXBContext.newInstance(ServicePriceRS.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			ServicePriceRS servicePrice =  (ServicePriceRS) unmarshaller.unmarshal(is);
+			assertEquals(expectedAmount, servicePrice.getDataLists().getServiceList().get(0).getPrice().get(0).getTotal().value);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+	}
+}
