e2dece3067d5c45316b1d213ca2d19c1f6752bc1
Juozas Rimša <juozas.rimsa@gmail.com>
2015-10-16 01:11:42 +0300

    Add JavaDoc for NdcClient

--- ndc-client/src/main/java/org/iata/ndc/NdcClient.java
+++ ndc-client/src/main/java/org/iata/ndc/NdcClient.java
@@ -13,65 +13,189 @@ import org.iata.ndc.schema.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * JAXB-based implementation of IATA's NDC standard.
+ *
+ * This implementation can handle a subset of all NDC calls.
+ */
 public class NdcClient {
 	private static final Logger LOG = LoggerFactory.getLogger(NdcClient.class);
+	/** NDC Client version */
 	private static final String VERSION = "0.1.0";
+	/** User agent passed to server */
 	private static final String USER_AGENT = "NDC Java Wrapper / " + VERSION;
-	private final String uri;
+	/** Server URL */
+	private final String url;
+	/** Authorization key */
 	private final String key;
+	/** ContentType for the request */
 	private final ContentType contentType;
 
-	public NdcClient(String serviceURI, String authorizationKey) {
-		this.uri = serviceURI;
+	/**
+	 * Creates client for querying a specific NDC service endpoint
+	 * @param serviceUrl url for NDC REST XML service
+	 * @param authorizationKey authorizationKey for service
+	 */
+	public NdcClient(String serviceUrl, String authorizationKey) {
+		this.url = serviceUrl;
 		this.key = authorizationKey;
 		this.contentType = ContentType.create(ContentType.APPLICATION_XML.getMimeType(), Consts.UTF_8);
 	}
 
-	public AirShoppingRS airShopping(AirShoppingRQ airShoppingRQ) throws ClientProtocolException, IOException {
+	/**
+	 * Sends AirShopping request to server, receives and processes the response.
+	 *
+	 * Request Object is marshalled into
+	 *
+	 * @param airShoppingRQ request Object
+	 * @return AirShoppingRS instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if marshalling the request or unmarshalling server response to Java Object fails
+	 */
+	public AirShoppingRS airShopping(AirShoppingRQ airShoppingRQ) throws ClientProtocolException, IOException, ClientException {
 		return marshalAndSend(airShoppingRQ, "AirShopping");
 	}
 
-	public FlightPriceRS flightPrice(FlightPriceRQ flightPriceRQ) throws ClientProtocolException, IOException {
+	/**
+	 * Sends FlightPrice request to server, receives and processes the response.
+	 *
+	 * @param flightPriceRQ request Object
+	 * @return FlightPriceRS instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if marshalling the request or unmarshalling server response to Java Object fails
+	 */
+	public FlightPriceRS flightPrice(FlightPriceRQ flightPriceRQ) throws ClientProtocolException, IOException, ClientException {
 		return marshalAndSend(flightPriceRQ, "FlightPrice");
 	}
 
-	public SeatAvailabilityRS seatAvailability(SeatAvailabilityRQ seatAvailabilityRQ) throws ClientProtocolException, IOException {
+	/**
+	 * Sends SeatAvailability request to server, receives and processes the response.
+	 *
+	 * @param seatAvailabilityRQ request Object
+	 * @return SeatAvailabilityRS instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if marshalling the request or unmarshalling server response to Java Object fails
+	 */
+	public SeatAvailabilityRS seatAvailability(SeatAvailabilityRQ seatAvailabilityRQ) throws ClientProtocolException, IOException, ClientException {
 		return marshalAndSend(seatAvailabilityRQ, "SeatAvailability");
 	}
 
-	public ServiceListRS serviceList(ServiceListRQ serviceListRQ) throws ClientProtocolException, IOException {
+	/**
+	 * Sends ServiceList request to server, receives and processes the response.
+	 *
+	 * @param serviceListRQ request Object
+	 * @return ServiceListRS instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if marshalling the request or unmarshalling server response to Java Object fails
+	 */
+	public ServiceListRS serviceList(ServiceListRQ serviceListRQ) throws ClientProtocolException, IOException, ClientException {
 		return marshalAndSend(serviceListRQ, "ServiceList");
 	}
 
-	public ServicePriceRS servicePrice(ServicePriceRQ servicePriceRQ) throws ClientProtocolException, IOException {
+	/**
+	 * Sends ServicePrice request to server, receives and processes the response.
+	 *
+	 * @param servicePriceRQ request Object
+	 * @return ServicePriceRS instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if marshalling the request or unmarshalling server response to Java Object fails
+	 */
+	public ServicePriceRS servicePrice(ServicePriceRQ servicePriceRQ) throws ClientProtocolException, IOException, ClientException {
 		return marshalAndSend(servicePriceRQ, "ServicePrice");
 	}
 
-	public OrderViewRS orderCreate(OrderCreateRQ orderCreateRQ) throws ClientProtocolException, IOException {
+	/**
+	 * Sends OrderCreate request to server, receives and processes the response.
+	 *
+	 * @param orderCreateRQ request Object
+	 * @return OrderViewRS instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if marshalling the request or unmarshalling server response to Java Object fails
+	 */
+	public OrderViewRS orderCreate(OrderCreateRQ orderCreateRQ) throws ClientProtocolException, IOException, ClientException {
 		return marshalAndSend(orderCreateRQ, "OrderCreate");
 	}
 
-	public OrderListRS orderList(OrderListRQ orderListRQ) throws ClientProtocolException, IOException {
+	/**
+	 * Sends OrderList request to server, receives and processes the response.
+	 *
+	 * @param orderListRQ request Object
+	 * @return OrderListRS instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if unmarshalling server response to Java Object fails
+	 */
+	public OrderListRS orderList(OrderListRQ orderListRQ) throws ClientProtocolException, IOException, ClientException {
 		return marshalAndSend(orderListRQ, "OrderList");
 	}
 
-	public OrderViewRS orderRetrieve(OrderRetrieveRQ orderRetrieveRQ) throws ClientProtocolException, IOException {
+	/**
+	 * Sends OrderRetrieve request to server, receives and processes the response.
+	 *
+	 * @param orderRetrieveRQ request Object
+	 * @return OrderViewRS instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if marshalling the request or unmarshalling server response to Java Object fails
+	 */
+	public OrderViewRS orderRetrieve(OrderRetrieveRQ orderRetrieveRQ) throws ClientProtocolException, IOException, ClientException {
 		return marshalAndSend(orderRetrieveRQ, "OrderRetrieve");
 	}
 
-	public OrderCancelRS orderCancel(OrderCancelRQ orderCancelRQ) throws ClientProtocolException, IOException {
+	/**
+	 * Sends OrderCancel request to server, receives and processes the response.
+	 *
+	 * @param orderCancelRQ request Object
+	 * @return OrderCancelRS instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if marshalling the request or unmarshalling server response to Java Object fails
+	 */
+	public OrderCancelRS orderCancel(OrderCancelRQ orderCancelRQ) throws ClientProtocolException, IOException, ClientException {
 		return marshalAndSend(orderCancelRQ, "OrderCancel");
 	}
 
-	public ItinReshopRS itinReshop(ItinReshopRQ itinReshopRQ) throws ClientProtocolException, IOException {
+	/**
+	 * Sends ItinReshop request to server, receives and processes the response.
+	 *
+	 * @param itinReshopRQ request Object
+	 * @return ItinReshopRS instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if marshalling the request or unmarshalling server response to Java Object fails
+	 */
+	public ItinReshopRS itinReshop(ItinReshopRQ itinReshopRQ) throws ClientProtocolException, IOException, ClientException {
 		return marshalAndSend(itinReshopRQ, "ItinReshop");
 	}
 
-	private <S, T> S marshalAndSend(T request, String method) throws ClientProtocolException, IOException {
+	/**
+	 * Internal method which is used by all request methods
+	 *
+	 * @param request request Object
+	 * @param method request method name
+	 * @return Object instance containing the response from server
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if marshalling the request or unmarshalling server response to Java Object fails
+	 */
+	private <S, T> S marshalAndSend(T request, String method) throws ClientProtocolException, IOException, ClientException {
 		return sendRequest(marshallRequest(request), method);
 	}
 
-	private <T> String marshallRequest(T request) {
+	/**
+	 * Marshals request object into String
+	 *
+	 * @param request request object
+	 * @return marshalled request
+	 * @throws ClientException if marshalling the request to XML fails
+	 */
+	private <T> String marshallRequest(T request) throws ClientException {
 		try {
 			JAXBContext context = JAXBContext.newInstance("org.iata.ndc.schema");
 			Marshaller marshaller = context.createMarshaller();
@@ -84,10 +208,20 @@ public class NdcClient {
 		}
 	}
 
-	private <T> T sendRequest(String request, String method) throws ClientProtocolException, IOException {
+	/**
+	 * Sends request and returns marshalled response Object.
+	 *
+	 * @param request String containing XML request
+	 * @param method request method name
+	 * @return Response object returned by sender
+	 * @throws ClientProtocolException if HTTP protocol exception is encountered during service call
+	 * @throws IOException if error is encountered while processing server response
+	 * @throws ClientException if unmarshalling server response to Java Object fails
+	 */
+	private <T> T sendRequest(String request, String method) throws ClientProtocolException, IOException, ClientException {
 		LOG.debug("{} request:\n{}", method, request);
 		return Request
-				.Post(uri)
+				.Post(url)
 				.userAgent(USER_AGENT)
 				.addHeader("Authorization-key", key)
 				.addHeader("X-NDC-Method", method)
