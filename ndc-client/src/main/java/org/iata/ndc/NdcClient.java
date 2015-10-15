5ff1c3d01c3ae88ed017319769ed98d6f6f033c4
Juozas Rimša <juozas.rimsa@gmail.com>
2015-10-16 00:17:56 +0300

    Simplify method calls in NdcClient

--- ndc-client/src/main/java/org/iata/ndc/NdcClient.java
+++ ndc-client/src/main/java/org/iata/ndc/NdcClient.java
@@ -28,53 +28,47 @@ public class NdcClient {
 	}
 
 	public AirShoppingRS airShopping(AirShoppingRQ airShoppingRQ) throws ClientProtocolException, IOException {
-		String request = marshallRequest(airShoppingRQ);
-		return sendRequest(request, "AirShopping");
+		return marshalAndSend(airShoppingRQ, "AirShopping");
 	}
 
 	public FlightPriceRS flightPrice(FlightPriceRQ flightPriceRQ) throws ClientProtocolException, IOException {
-		String request = marshallRequest(flightPriceRQ);
-		return sendRequest(request, "FlightPrice");
+		return marshalAndSend(flightPriceRQ, "FlightPrice");
 	}
 
 	public SeatAvailabilityRS seatAvailability(SeatAvailabilityRQ seatAvailabilityRQ) throws ClientProtocolException, IOException {
-		String request = marshallRequest(seatAvailabilityRQ);
-		return sendRequest(request, "SeatAvailability");
+		return marshalAndSend(seatAvailabilityRQ, "SeatAvailability");
 	}
 
 	public ServiceListRS serviceList(ServiceListRQ serviceListRQ) throws ClientProtocolException, IOException {
-		String request = marshallRequest(serviceListRQ);
-		return sendRequest(request, "ServiceList");
+		return marshalAndSend(serviceListRQ, "ServiceList");
 	}
 
-	public ServicePriceRS servicePrice(ServicePriceRQ serviceListRQ) throws ClientProtocolException, IOException {
-		String request = marshallRequest(serviceListRQ);
-		return sendRequest(request, "ServicePrice");
+	public ServicePriceRS servicePrice(ServicePriceRQ servicePriceRQ) throws ClientProtocolException, IOException {
+		return marshalAndSend(servicePriceRQ, "ServicePrice");
 	}
 
 	public OrderViewRS orderCreate(OrderCreateRQ orderCreateRQ) throws ClientProtocolException, IOException {
-		String request = marshallRequest(orderCreateRQ);
-		return sendRequest(request, "OrderCreate");
+		return marshalAndSend(orderCreateRQ, "OrderCreate");
 	}
 
 	public OrderListRS orderList(OrderListRQ orderListRQ) throws ClientProtocolException, IOException {
-		String request = marshallRequest(orderListRQ);
-		return sendRequest(request, "OrderList");
+		return marshalAndSend(orderListRQ, "OrderList");
 	}
 
 	public OrderViewRS orderRetrieve(OrderRetrieveRQ orderRetrieveRQ) throws ClientProtocolException, IOException {
-		String request = marshallRequest(orderRetrieveRQ);
-		return sendRequest(request, "OrderRetrieve");
+		return marshalAndSend(orderRetrieveRQ, "OrderRetrieve");
 	}
 
 	public OrderCancelRS orderCancel(OrderCancelRQ orderCancelRQ) throws ClientProtocolException, IOException {
-		String request = marshallRequest(orderCancelRQ);
-		return sendRequest(request, "OrderCancel");
+		return marshalAndSend(orderCancelRQ, "OrderCancel");
 	}
 
 	public ItinReshopRS itinReshop(ItinReshopRQ itinReshopRQ) throws ClientProtocolException, IOException {
-		String request = marshallRequest(itinReshopRQ);
-		return sendRequest(request, "ItinReshop");
+		return marshalAndSend(itinReshopRQ, "ItinReshop");
+	}
+
+	private <S, T> S marshalAndSend(T request, String method) throws ClientProtocolException, IOException {
+		return sendRequest(marshallRequest(request), method);
 	}
 
 	private <T> String marshallRequest(T request) {
