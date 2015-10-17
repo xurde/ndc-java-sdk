9a2170b077e35800297821b30d3452e5dbb6579b
Juozas Rimša <juozas.rimsa@gmail.com>
2015-10-17 16:04:30 +0300

    Update OrderCreateRQ and OrderViewRS unmarshalling tests

--- ndc-client/src/test/java/org/iata/ndc/schema/OrderViewRSUnmarshallingIT.java
+++ ndc-client/src/test/java/org/iata/ndc/schema/OrderViewRSUnmarshallingIT.java
@@ -1,32 +1,38 @@
 package org.iata.ndc.schema;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
-import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collection;
 
-import javax.xml.bind.*;
+import javax.xml.bind.JAXBException;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
-@Ignore
 @RunWith(Parameterized.class)
-public class OrderViewRSUnmarshallingIT {
+public class OrderViewRSUnmarshallingIT extends AbstractUnmarshaller<OrderViewRS> {
 
-	@Parameters(name = "{index}: {0} {1}")
+	@Parameters(name = "{index}: {0}")
 	public static Collection<String[]> sampleFiles() {
 		return Arrays.asList(new String[][] {
-			{"/OrderViewRS for OrderCreate - OneWay with one pax.xml", "1#C#109987187#110031494"},
-			{"/OrderViewRS for OrderCreate - Roundtrip with one pax.xml", "1#M#108183266"},
-			{"/OrderViewRS of OrderChange  - AddPassenger - OneWay.xml", "1#C#109987187#110031494"},
-			{"/OrderViewRS of OrderChange  - AddPassenger - RoundTrip.xml", "1#M#108183266"}
+			{"/Athena/OneWay/OrderViewRS - OrderCreateRQ - OneWay with one pax.xml", "1#M#111198795#111162714"},
+			{"/Athena/RoundTrip/OrderViewRS - OrderCreateRQ - Round Trip with one pax.xml", "1#M#111186373#111192206"},
+			{"/Kronos/OneWay/OrderViewRS - OrderCreate.xml", "1#M#109939774#109952838"},
+			{"/Kronos/RoundTrip/OrderViewRS - OrderCreateRQ.xml", "1#M#109953106#109939177"},
+
+			{"/Athena/OneWay/OrderViewRS - OrderChangeRQ.xml", "1#M#111198795#111162714"},
+			{"/Athena/RoundTrip/OrderViewRS - OrderChangeRQ - Round Trip with one pax.xml", "1#M#111186373#111192206"},
+			{"/Kronos/OneWay/OrderViewRS - OrderChange.xml", "1#M#109939774#109952838"},
+			{"/Kronos/RoundTrip/OrderViewRS - OrderChangeRQ.xml", "2#M#109953106#109939177"},
+
+			{"/Athena/OneWay/OrderViewRS - OrderRetrieveRQ.xml", "1#M#111198795#111162714"},
+			{"/Athena/RoundTrip/OrderViewRS - OrderRetrieveRQ.xml", "1#M#111186373#111192206"},
+			{"/Kronos/OneWay/OrderViewRS - OrderChange.xml", "1#M#109939774#109952838"},
+			{"/Kronos/RoundTrip/OrderViewRS - OrderRetrieveRQ.xml", "2#M#109953106#109939177"}
 		});
 	}
 
@@ -37,16 +43,8 @@ public class OrderViewRSUnmarshallingIT {
 	public String offerItemID;
 
 	@Test
-	public void unmarshal() {
-		InputStream is = this.getClass().getResourceAsStream(resource);
-		try {
-			JAXBContext context = JAXBContext.newInstance(OrderViewRS.class);
-			Unmarshaller unmarshaller = context.createUnmarshaller();
-			OrderViewRS orderViewRS =  (OrderViewRS) unmarshaller.unmarshal(is);
-			assertEquals(offerItemID, orderViewRS.getResponse().getOrder().get(0).getOrderItems().getOrderItem().get(0).getOrderItemID().getValue());
-		} catch (JAXBException e) {
-			e.printStackTrace();
-			fail(e.toString());
-		}
+	public void unmarshal() throws JAXBException {
+		OrderViewRS orderViewRS = unmarshal(resource);
+		assertEquals(offerItemID, orderViewRS.getResponse().getOrder().get(0).getOrderItems().getOrderItem().get(0).getOrderItemID().getValue());
 	}
 }
